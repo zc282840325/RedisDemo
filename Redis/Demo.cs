7e6ecd57f3a6c4dc1a0bc4ce2125ce6a9824ccd8
Administrator <282840325@qq.com>
2019-08-02 18:44:52 +0800

    Redies入门级增，删，改，查

---
+++ Redis/Demo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Redis
+{
+   public class Demo
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Sex { get; set; }
+        public int Age { get; set; }
+    }
+}
--- Redis/Program.cs
+++ Redis/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ServiceStack.Redis;
+using ServiceStack.Text;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,29 @@ namespace Redis
     {
         static void Main(string[] args)
         {
+            var redisManger = new RedisManagerPool("127.0.0.1:6379");
+            var redis = redisManger.GetClient();
+            var redisDemos = redis.As<Demo>();
+            var newDemo = new Demo { ID=1,Name="zc",Age=28,Sex="男"};
+            var newDemo2 = new Demo { ID = 2, Name = "zc", Age = 28, Sex = "男" };
+
+            redisDemos.Store(newDemo);
+            redisDemos.Store(newDemo2);
+            var alldemo = redisDemos.GetAll();
+            Demo saveTodo = redisDemos.GetById(newDemo.ID);              
+            "Saved Todo: {0}".Print(alldemo.Dump());
+
+            saveTodo.Name = "zc132";
+            redisDemos.Store(saveTodo);
+
+            var updateDemo = redisDemos.GetById(newDemo.ID);
+            "Update Todo: {0}".Print(updateDemo.Dump());
+
+            redisDemos.DeleteById(newDemo.ID);
+
+            var remainDemo = redisDemos.GetAll();
+            "All Todo: {0}".Print(remainDemo.Dump());
+
         }
     }
 }
